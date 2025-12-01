2e71906af2aa17eea5dff5c62ce9a5b6d22fe8ab
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-01 20:58:52 +0100

    Added event when clikcing start div is hidden and quiz div is displayed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import { questions } from './questions.js';
-console.log(questions[0].question)
+
+const startScreen = document.getElementById('start-screen')
+const quizScreen = document.getElementById('quiz-screen')
+
+const startBtn = document.getElementById('start-btn')
+
+
+let questionIndex;
+let timer;
+
+startBtn.addEventListener('click', () => {
+    startScreen.style.display = 'none'
+    quizScreen.style.display = 'block'
+
+    questionIndex = 0
+    timer = 0
+})
