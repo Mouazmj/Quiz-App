6deb7b541507d7b30a4274d7074df010e747149c
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-01 21:59:44 +0100

    Adding functinality when clikcing one of the ansers so the code goes to next question

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { questions } from './questions.js';
 
 const startScreen = document.getElementById('start-screen')
 const quizScreen = document.getElementById('quiz-screen')
+const resultScreen = document.getElementById('result-screen')
 
 const startBtn = document.getElementById('start-btn')
 
@@ -11,6 +12,7 @@ const questionBtns = document.querySelectorAll('.btn-answer')
 
 let questionIndex;
 let timer;
+let points;
 
 startBtn.addEventListener('click', () => {
     startScreen.style.display = 'none'
@@ -18,10 +20,27 @@ startBtn.addEventListener('click', () => {
 
     questionIndex = 0
     timer = 0
+    points = 0
 
     questionText.textContent = questions[questionIndex].question
 
     for (let i = 0; i < questionBtns.length; i++) {
         questionBtns[i].textContent = questions[questionIndex].answers[i].text;
+
+        questionBtns[i].addEventListener('click', () => {
+            if (questions[questionIndex].answers[i].correct === true) {
+                points += 1
+            }
+            questionIndex += 1
+            if (questionIndex >= questions.length) {
+                quizScreen.style.display = 'none'
+                resultScreen.style.display = 'block'
+            } else {
+                questionText.textContent = questions[questionIndex].question
+                for (let i = 0; i < questionBtns.length; i++) {
+                    questionBtns[i].textContent = questions[questionIndex].answers[i].text
+                }
+            }
+        })
     }    
 })
