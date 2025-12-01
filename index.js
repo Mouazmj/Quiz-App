bbcb38ccc89d976db70e458f2a19c150ec76c112
Mouaz Jamal <mj225ih@student.lnu.se>
2025-12-01 21:14:39 +0100

    Adding the text for questions and for buttons

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const quizScreen = document.getElementById('quiz-screen')
 
 const startBtn = document.getElementById('start-btn')
 
+const questionText = document.getElementById('question-text')
+const questionBtns = document.querySelectorAll('.btn-answer')
+
 
 let questionIndex;
 let timer;
@@ -15,4 +18,10 @@ startBtn.addEventListener('click', () => {
 
     questionIndex = 0
     timer = 0
+
+    questionText.textContent = questions[questionIndex].question
+
+    for (let i = 0; i < questionBtns.length; i++) {
+        questionBtns[i].textContent = questions[questionIndex].answers[i].text;
+    }    
 })
